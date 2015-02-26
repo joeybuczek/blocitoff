05015e491d42898284a305a327e68c6bf2860bc1
Joey Buczek <joe.buczek@gmail.com>
2015-02-26 03:28:02 +0000

    remove edit

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-Rails::Initializer.run do |config|
-  config.gem 'javan-whenever', :lib => false, :source => 'http://gem.github.com'
-end
-
 # Initialize the Rails application.
 Rails.application.initialize!
